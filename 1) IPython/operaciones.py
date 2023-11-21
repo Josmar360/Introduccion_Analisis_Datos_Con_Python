41942133997846ba9329eb3e1fdc4e17db9c2c25
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-11-20 19:03:57 -0600

    Crear archivos con IPython

---
+++ 1) IPython/operaciones.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+def resta(a: int, b: int) -> int:
+    """Permite restar 2 números enteros."""
+    return a-b
+    
+resultado = resta(20, 10)
+def suma(a: int, b: int) -> int:
+    """Permite sumar 2 números enteros."""
+    return a+b
+    
+resultado = suma(10, 20)
+print(resultado)
