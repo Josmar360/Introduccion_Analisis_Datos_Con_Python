9326a1359530ca6575b8654160de873436690f67
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-11-20 19:47:35 -0600

    Obtener y actualizar elementos

---
+++ 2) Numpy/4) Obtener_actualizar_elementos.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+import numpy as np
+
+# Crear un arreglo random entero
+a = np.random.randint(0, 10, 10)
+
+# Ver el primer numero
+a[0]
+
+# Ver el ultimo numero
+a[-1]
+
+# Cambiar o actualizar un elemento
+a[0] = 100
+
+print(a)
