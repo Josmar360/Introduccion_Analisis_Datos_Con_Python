d74db783b35f65c9338a5cf6f2541c5522971016
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-11-20 19:19:21 -0600

    Operaciones con arreglos

---
+++ 2) Numpy/Operaciones_Arreglo.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import numpy as np
+
+np.array( [1, 2, 3, 4, 5] )
+
+a = np.array( [1, 2, 3, 4, 5] )
+"""
+Operaciones que se pueden hacer con arreglos
+a + 10
+a - 10
+a * 10
+a / 10
+"""
+
+b = np.array( [6, 7, 8, 9, 10] )
+"""
+Operaciones que se pueden hacer con dos arreglos
+a + b
+a * b
+a / b
+a - b
+"""
