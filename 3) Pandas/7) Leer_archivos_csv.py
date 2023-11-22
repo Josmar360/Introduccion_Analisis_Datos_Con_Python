535465d6f62dc62540eebaba655d09295bbc8561
Josmar360 <josmargustavopalominocastelan@gmail.com>
2023-11-21 23:18:15 -0600

    Abrir archivo csv y usar metodos head y tail

--- 3) Pandas/7) Leer_archivos_csv.py
+++ 3) Pandas/7) Leer_archivos_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Abrir el archivo user.csv
 print("====== Imprimir el archivo csv ======")
-data = pd.read_csv("3) Pandas/7) Leer_archivos_csv/7) Users.csv", index_col='id')
+data = pd.read_csv("3) Pandas/7) Users.csv", index_col='id')
 print(data)
 
 # Usar metodos head()
